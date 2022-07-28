1b5e38c2a6826af9d0b5c23e39e610dcf068e43e
Luc0903 <87391029+Luc0903@users.noreply.github.com>
2022-07-28 16:46:35 -0300

    Elimination I deleted some console.logs and a link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Wrapper from "./components/Wrapper";
 import "./styles.css";
-//https://ch-button-component-h-yoshikawa44.vercel.app/
+
 export default function App() {
   return (
     <div className="App">
